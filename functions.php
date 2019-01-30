96aa410a957ca438e14696ed2dc2b7946d59d9e2
riforik <isaiah.robinson1029@gmail.com>
2019-01-29 19:17:55 -0500

    First Commit
    Committing original theme files

---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Time Tells Tech functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Time_Tells_Tech
+ */
+
+ /**
+  * Theme Setup
+  */
+	require get_template_directory() . '/inc/setup.php';
+
+/**
+ * Enqueue scripts and styles.
+ */
+function time_tells_tech_scripts() {
+	wp_enqueue_style( 'time-tells-tech-style', get_stylesheet_uri() );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'time_tells_tech_scripts' );
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
