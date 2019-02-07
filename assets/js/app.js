ad527c7479d8814a86a5b76488d5ea8ff473d406
riforik <isaiah.robinson1029@gmail.com>
2019-02-07 14:03:37 -0500

    Improving Location
    - general styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,20 +22,24 @@
       id: 'mapbox.streets'
     }).addTo(mymap);
 
-    L.marker([43.848900, -79.020986]).addTo(mymap)
-      .bindPopup(`<div class='mapPopUp'><h1>Hello world!</h1>\nI am a popup.</div>`).openPopup();
+    L.marker([43.894260, -78.868295]).addTo(mymap)
+      .bindPopup(`<div class='mapPopUp'><h1>Durham Community Legal Clinic</h1><h2>200 John Street West, Unit B1, Oshawa</h2><h3>By apointment only</h4><h3>call 905-728-7321 ext. 259</h3></div>`);
 
-    L.circle([43.849875, -79.035955], 500, {
+    // Diverse Financial Group
+    L.marker([43.848900, -79.020986]).addTo(mymap)
+      .bindPopup(`<div class='mapPopUp'><h1>Diverse Financial Group</h1><h2>190 Harwood Avenue South (inside G Centre), Ajax</h2><h3>By apointment only</h3><h4>Current and prior year returns</h4><h3>call 647-887-8725</h3></div>`).openPopup();
+
+    L.circle([43.849875, -79.035955], 250, {
       color: 'red',
       fillColor: '#f03',
       fillOpacity: 0.5
     }).addTo(mymap).bindPopup("I am a circle.");
 
-    // L.polygon([
-    //   [43.8489, -79.020986],
-    //   [51.503, -0.06],
-    //   [51.51, -0.047]
-    // ]).addTo(mymap).bindPopup("I am a polygon.");
+    L.polygon([
+      [43.847079, -79.021547],
+      [43.846071, -79.019101],
+      [43.848038, -79.018435]
+    ]).addTo(mymap).bindPopup("I am a polygon.");
 
 
     var popup = L.popup();
@@ -48,6 +52,23 @@
     }
 
     mymap.on('click', onMapClick);
+
+    // -------     C O N T R O L L E R     -------
+    $(document).on("click", "body .map-list-option", function() {
+
+      var id = $(this).attr("data-id");
+      mymap.setView(new L.LatLng(43.894260, -78.868295), 16, {
+        animation: true
+      });
+
+      console.log(mymap.marker);
+
+      console.log(id);
+    });
   });
+
+
+
+
 
 })(jQuery);
--- template-parts/content-mine.php
+++ template-parts/content-mine.php
@@ -13,28 +13,33 @@
 	<section class="entry-header">
 		<?php
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h4 class="entry-title">', '</h4>' );
 		else :
-			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+			the_title( '<h4 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' );
 		endif;
 	 ?>
 	 <div id="map-section">
 		 <div id="map-list">
-		  <ul>
-		 	 <li>one</li>
-		 	 <li>two</li>
-		 	 <li>three</li>
-		  </ul>
+			 <div class="map-list-option" data-id="001">
+				 <h3>Diverse Financial Group</h3>
+ 				<h4>190 Harwood Avenue South (inside G Centre), Ajax</h4>
+			 </div>
+			 <div class="map-list-option" data-id="002">
+				 <h3>Durham Community Legal Clinic</h3>
+ 				<h4>200 John Street West, Unit B1, Oshawa</h4>
+			 </div>
 		 </div>
-		 
+
 		 <div id="map-box">
 		 	<div id="leafletMap"></div>
 		 </div>
 
 		 <div id="map-info">
-		 	<h1>Title</h1>
-			<h2>Address</h2>
-			<h3>more info</h3>
+		 	<h1>Diverse Financial Group</h1>
+			<h2>190 Harwood Avenue South (inside G Centre), Ajax</h2>
+			<p>By apointment only</p>
+			<p>Current and prior year returns</p>
+			<p>call 647-887-8725</p>
 		 </div>
 	 </div>
 
